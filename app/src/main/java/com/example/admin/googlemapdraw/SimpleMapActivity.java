deb5bf502935f711633bc37b04d7905c10f1404e
tamasan810 <wataru.abe.5n@stu.hosei.ac.jp>
2016-06-27 17:07:38 +0900

    解決しました。

--- app/src/main/java/com/example/admin/googlemapdraw/SimpleMapActivity.java
+++ app/src/main/java/com/example/admin/googlemapdraw/SimpleMapActivity.java
@@ -1,19 +1,21 @@
 package com.example.admin.googlemapdraw;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SimpleMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private GoogleMap mMap;
 
     @Override
@@ -26,38 +28,85 @@ public class SimpleMapActivity extends FragmentActivity implements OnMapReadyCal
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private void requestAccessFineLocation() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng tokyo = new LatLng(35, 139);
-        mMap.addMarker(new MarkerOptions().position(tokyo).title("Marker in Tokyo"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(tokyo));
-        //現在の位置情報を表示
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+//        LatLng tokyo = new LatLng(35, 139);
+//        mMap.addMarker(new MarkerOptions().position(tokyo).title("Marker in Tokyo"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(tokyo));
+        // DangerousなPermissionはリクエストして許可をもらわないと使えない
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                // 一度拒否されたとき、Rationale(理論的根拠)を説明して、再度許可ダイアログを出すようにする
+                new AlertDialog.Builder(this)
+                        .setTitle("許可が必要です")
+                        .setMessage("移動に合わせて地図を動かすためには、ACCESS_FINE_LOCATIONを" +
+                                "許可してください")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // OK button pressed
+                                requestAccessFineLocation();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                showToast("GPS機能が使えないので、地図は動きません");
+                            }
+                        })
+                        .show();
+            } else{
+                // まだ許可を求める前のとき、許可を求めるダイアログを表示する
+                requestAccessFineLocation();
+            }
+        } else {
+            // 許可されている場合
+            myLocationEnable();
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // ユーザーが許可したとき
+                // 許可が必要な機能を改めて実行する
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    myLocationEnable();
+                }
+                else {
+                    // ユーザーが許可しなかった時
+                    // 許可されなかったため機能が実行できないことを表示する
+                    showToast("GPS機能が使えないので、地図は動きません");
+                    // 以下を実行すると、java.lang.RuntimeExceptionになる
+                    // mMap.setMyLocationEnable(true);
+                }
+                return;
+            }
+        }
+    }
+
+    private void myLocationEnable(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         mMap.setMyLocationEnabled(true);
     }
+    private void showToast(String msg) {
+        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        error.show();
+    }
+
+
 }
