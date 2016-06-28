ec67d26d4945f8edd8bbe8c996e49dab13b92b81
tamasan810 <wataru.abe.5n@stu.hosei.ac.jp>
2016-06-28 19:04:05 +0900

    地図が表示されない不具合を解決しました。

--- app/src/main/java/com/example/admin/googlemapdraw/SimpleMapActivity.java
+++ app/src/main/java/com/example/admin/googlemapdraw/SimpleMapActivity.java
@@ -2,11 +2,19 @@ package com.example.admin.googlemapdraw;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -17,6 +25,7 @@ public class SimpleMapActivity extends FragmentActivity implements OnMapReadyCal
 
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private GoogleMap mMap;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,7 @@ public class SimpleMapActivity extends FragmentActivity implements OnMapReadyCal
 //        LatLng tokyo = new LatLng(35, 139);
 //        mMap.addMarker(new MarkerOptions().position(tokyo).title("Marker in Tokyo"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(tokyo));
+
         // DangerousなPermissionはリクエストして許可をもらわないと使えない
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -82,7 +92,11 @@ public class SimpleMapActivity extends FragmentActivity implements OnMapReadyCal
                 // ユーザーが許可したとき
                 // 許可が必要な機能を改めて実行する
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 現在地
                     myLocationEnable();
+
+//                    // 位置測定を始める
+//                    locationStart();
                 }
                 else {
                     // ユーザーが許可しなかった時
@@ -97,6 +111,7 @@ public class SimpleMapActivity extends FragmentActivity implements OnMapReadyCal
     }
 
     private void myLocationEnable(){
+        Log.d("debug", "myLocationEnable()");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             return;
@@ -108,5 +123,70 @@ public class SimpleMapActivity extends FragmentActivity implements OnMapReadyCal
         error.show();
     }
 
+    private void locationStart() {
+        Log.d("debug", "locationStart()");
 
+        // LocationManagerインスタンス生成
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            // GPSを設定するように促す
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(settingsIntent);
+            Log.d("debug", "gpsEnable, startActivity");
+        } else {
+            Log.d("debug", "gpsEnabled");
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+            Log.d("debug", "checkSelfPermission false");
+            return;
+        }
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // 経度の表示
+                TextView textView1 = (TextView) findViewById(R.id.textView1);
+                textView1.setText("Latitude:" + location.getLatitude());
+
+                // 緯度の表示
+                TextView textView2 = (TextView) findViewById(R.id.textView2);
+                textView2.setText("Longitude:" + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                switch (status) {
+                    case LocationProvider.AVAILABLE:
+                        Log.d("debug", "LocationProvider.AVAILABLE");
+                        break;
+                    case LocationProvider.OUT_OF_SERVICE:
+                        Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                        break;
+                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                        Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                        break;
+                }
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, locationListener);
+    }
 }
